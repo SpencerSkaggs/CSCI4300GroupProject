cf7996694de1e20fa81339736039e61988324c4c
aj1037243 <83550136+aj1037243@users.noreply.github.com>
2022-11-09 20:18:07 -0500

    Add files via upload

---
+++ LoginInfo.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function LoginInfo() {
+  return (
+    <div className='Login'>
+
+     <h1> Good Afternoon. Welcome to our tourist app.</h1>
+        <br />
+       <p>What is your name and Password so we may get started swiftly. Thank you again
+       </p>
+
+      <p>Username</p>
+      <input type="text" />
+      <p>Password</p>
+      <input type="text" />
+      <br />
+      <button type="submit">  submit</button>    
+
+
+
+    </div>
+  )
+}
+
+export default LoginInfo
